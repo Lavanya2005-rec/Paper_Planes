433c3de375a667899d72eac20aae785dbc90f16e
Lavanya2005-rec <231001099@rajalakshmi.edu.in>
2025-04-13 16:33:46 +0530

    Add files via upload

---
+++ AI/schemas.py
@@ -0,0 +1,9 @@
+# schemas.py
+
+REQUIRED_COLUMNS = [
+    "user_id", "transaction_id", "transaction_amount", "location", "ip_address",
+    "device_type", "vpn_status", "mac_address", "geo_lat", "geo_long",
+    "kyc_status", "transaction_time", "user_name", "user_email", "card_last4",
+    "device_id", "geo_anomaly", "ip_risk", "velocity_count", "velocity_flag",
+    "past_avg_spend", "user_agent", "hour", "is_night", "location_encoded"
+]
